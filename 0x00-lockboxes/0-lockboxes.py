6b168802ccb8dc7cda57b8a4521c702fa3a3108b
chiheblahouli <chiheb.lahouli@gmail.com>
2021-08-20 12:27:54 +0100

    c

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""0-lockboxes"""
+
+
+def canUnlockAll(boxes):
+    """ i can do all """
+    if len(boxes) == 0:
+        return False
+    if not isinstance(boxes, list):
+        return False
+    else:
+        lenbox = len(boxes)
+    arr = [0]
+    j = 0
+    for i in arr:
+        try:
+            for j in boxes[i]:
+                if isinstance(j, int):
+                    if j < lenbox and j not in arr and j != 0 and j is not i:
+                        arr.append(j)
+        except BaseException:
+            pass
+    return len(arr) == lenbox
