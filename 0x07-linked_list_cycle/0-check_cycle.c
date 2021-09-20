c93dc601e49a892b7d95fcf7590aa24285d68a98
chiheblahouli <chiheb.lahouli@gmail.com>
2021-09-20 12:30:31 +0100

    c

---
+++ 0x07-linked_list_cycle/0-check_cycle.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+/**
+ * check_cycle - check_cycle
+ * @list: linked list
+ * Return: 1 or 0
+ */
+
+int check_cycle(listint_t *list)
+{
+listint_t *a, *b;
+
+if (!list)
+return (0);
+if (list->next == NULL)
+return (0);
+
+a = list->next;
+b = list->next->next;
+
+while (a && b && b->next)
+{
+if (a == b->next)
+return (1);
+a = a->next;
+b = b->next->next;
+}
+return (0);
+}
