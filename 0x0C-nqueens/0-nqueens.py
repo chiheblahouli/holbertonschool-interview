b8cfffa7b6bed517d9c45318e4f1bbf11ad956cb
chiheblahouli <chiheb.lahouli@gmail.com>
2022-01-13 16:52:24 +0100

    c

--- 0x0C-nqueens/0-nqueens.py
+++ 0x0C-nqueens/0-nqueens.py
@@ -1,44 +1,28 @@
-!/usr/bin/python3
-""" N queens """
+#!/usr/bin/python3
 import sys
-
-
-if len(sys.argv) > 2 or len(sys.argv) < 2:
-    print("Usage: nqueens N")
+if len(sys.argv) != 2:
+    print('Usage: nqueens N')
     exit(1)
-
-if not sys.argv[1].isdigit():
-    print("N must be a number")
+if sys.argv[1].isdigit() is False:
+    print('N must be a number')
     exit(1)
-
-if int(sys.argv[1]) < 4:
-    print("N must be at least 4")
+N = int(sys.argv[1])
+if (N < 4):
+    print('N must be at least 4')
     exit(1)
-
-n = int(sys.argv[1])
-
-
-def queens(n, i=0, a=[], b=[], c=[]):
-    """ find possible positions """
-    if i < n:
-        for j in range(n):
-            if j not in a and i + j not in b and i - j not in c:
-                yield from queens(n, i + 1, a + [j], b + [i + j], c + [i - j])
-    else:
-        yield a
-
-
-def solve(n):
-    """ solve """
-    k = []
-    i = 0
-    for solution in queens(n, 0):
-        for s in solution:
-            k.append([i, s])
-            i += 1
-        print(k)
-        k = []
-        i = 0
-
-
-solve(n)
+y = 0
+frequence_y = 2
+for i in range(N - 2):
+    Lists = []
+    y += 1
+    n = y
+    for x in range(N):
+        Lists.append([x, y])
+        if (y + frequence_y > N-1):
+            y += frequence_y - 1 - N
+        else:
+            y += frequence_y
+    print(Lists)
+    frequence_y += 1
+    y = n
+exit(0)
